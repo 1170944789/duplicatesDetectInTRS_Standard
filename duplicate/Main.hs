2b8f87c0661ffa8386fd22ad10c31c0240e93a30
1170944789 <49107074+1170944789@users.noreply.github.com>
2025-01-17 16:17:36 +0100

    Update Main.hs

--- duplicate/Main.hs
+++ duplicate/Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -prof -fprof-auto -rtsopts #-}
 module Main where
 
 import System.Directory
@@ -34,7 +35,7 @@ instance Hashable Term where
 instance Hashable TRS where
     hashWithSalt _ = hashValue
 
-type Rule = (Term, Term)
+type Rule = (Term, Term, Maybe Int)
 
 data TRSError = 
     InvalidFunctionName String Int    
@@ -45,31 +46,28 @@ data TRSError =
     deriving (Show, Eq)
 
 getFunsInRule :: Rule -> [String]
-getFunsInRule (lhs, rhs) = getFunsInTerm lhs ++ getFunsInTerm rhs
+getFunsInRule (lhs, rhs, _) = getFunsInTerm lhs ++ getFunsInTerm rhs
 
 getFunsInTerm :: Term -> [String]
 getFunsInTerm (Var _) = []
 getFunsInTerm (Fun f args) = f : concatMap getFunsInTerm args
 
 getRuleStructure :: Rule -> String
-getRuleStructure rule@(lhs, rhs) = 
-    let
-        getTermStructure :: Term -> String
-        getTermStructure (Var v) = "V"
+getRuleStructure (lhs, rhs, cost) = 
+    let getTermStructure (Var v) = "V"
         getTermStructure (Fun _ args) = "F" ++ show (length args) ++ 
                                       concatMap getTermStructure args
 
         vars = nub (collectVars lhs ++ collectVars rhs)
         varMap = Map.fromList (zip vars (map (\i -> "x" ++ show i) [1..]))
 
-        normalizeForStructure :: Term -> Term
         normalizeForStructure (Var v) = Var (varMap Map.! v)
-        normalizeForStructure (Fun _ args) = 
-            Fun "F" (map normalizeForStructure args)
+        normalizeForStructure (Fun _ args) = Fun "F" (map normalizeForStructure args)
 
         normLhs = normalizeForStructure lhs
         normRhs = normalizeForStructure rhs
-    in show (normLhs, normRhs)
+        costStr = maybe "" (\c -> ":cost" ++ show c) cost
+    in show (normLhs, normRhs) ++ costStr
 
 reportProgress :: String -> Int -> Int -> IO ()
 reportProgress phase current total = do
@@ -77,6 +75,18 @@ reportProgress phase current total = do
     printf "\r%s: %d/%d (%.2f%%) " phase current total percentage
     hFlush stdout
     when (current == total) (putStrLn "")
+
+generateOutputNames :: FilePath -> (FilePath, FilePath, FilePath)
+generateOutputNames dir = 
+    let baseName = map replaceSlash (takeBaseName dir)
+        duplicateFile = "duplicate_files_" ++ baseName ++ ".txt"
+        errorFile = "errors_" ++ baseName ++ ".txt"
+        performanceFile = "performance_" ++ baseName ++ ".txt"
+    in (duplicateFile, errorFile, performanceFile)
+  where
+    replaceSlash '/' = '_'
+    replaceSlash '\\' = '_'
+    replaceSlash c = c
 
 checkTRS :: String -> [TRSError]
 checkTRS input = 
@@ -95,62 +105,63 @@ checkFunctionNames :: [(Int, String)] -> [TRSError]
 checkFunctionNames lines = 
     [ InvalidFunctionName fname lineNum 
     | (lineNum, line) <- lines,
-        let parts = words line,
-        length parts >= 2,
-        let fname = takeWhile (/= ')') (dropWhile (=='(') (parts !! 1)),
-        any (\c -> c `elem` ['(', ')', ';']) fname || null fname
+      let parts = words line,
+      length parts >= 2,
+      let fname = takeWhile (/= ')') (dropWhile (=='(') (parts !! 1)),
+      any (\c -> c `elem` ['(', ')', ';']) fname || null fname
     ]
 
 checkArities :: [(Int, String)] -> [TRSError]
 checkArities lines =
     [ InvalidArity fname lineNum arityStr
     | (lineNum, line) <- lines,
-        isPrefixOf "(fun" line,
-        let parts = words line,
-        length parts >= 3,
-        let fname = takeWhile (/= ')') (dropWhile (=='(') (parts !! 1)),
-        let arityStr = takeWhile (/= ')') (parts !! 2),
-        let parseResult = reads arityStr :: [(Int, String)],
-        null parseResult || case parseResult of
-                            [(n, "")] -> n < 0 
-                            _ -> True
+      isPrefixOf "(fun" line,
+      let parts = words line,
+      length parts >= 3,
+      let fname = takeWhile (/= ')') (dropWhile (=='(') (parts !! 1)),
+      let arityStr = takeWhile (/= ')') (parts !! 2),
+      let parseResult = reads arityStr :: [(Int, String)],
+      null parseResult || case parseResult of
+                          [(n, "")] -> n < 0 
+                          _ -> True
     ]
 
 checkFunctionStructures :: [(Int, String)] -> [TRSError]
 checkFunctionStructures lines =
     [ InvalidFunctionStructure fname lineNum line
     | (lineNum, line) <- lines,
-        isPrefixOf "(fun" line,
-        let parts = words line,
-        not (length parts == 3),
-        let fname = if length parts >= 2 
-                then takeWhile (/= ')') (dropWhile (=='(') (parts !! 1))
-                else ""
+      isPrefixOf "(fun" line,
+      let parts = words line,
+      not (length parts == 3),
+      let fname = if length parts >= 2 
+              then takeWhile (/= ')') (dropWhile (=='(') (parts !! 1))
+              else ""
     ]
 
 checkRuleStructures :: [(Int, String)] -> [TRSError]
 checkRuleStructures lines = 
     [ InvalidRuleStructure lineNum line
     | (lineNum, line) <- lines,
-        isPrefixOf "(rule" line,
-        let withoutOuterParens = init (tail (trim line))
-            afterRule = drop 4 withoutOuterParens
-            terms = splitTermsInRule (trim afterRule)
-        in case terms of
-            [_, _] -> False
-            _ -> True
+      isPrefixOf "(rule" line,
+      let withoutOuterParens = init (tail (trim line))
+          afterRule = dropWhile isSpace (drop 4 withoutOuterParens)
+          terms = splitTermsInRule (trim afterRule)
+      in not (case terms of
+          [_, _] -> True
+          [_, _, ":cost 0"] -> True
+          _ -> False)
     ]
 
 checkVariableNames :: [(Int, String)] -> [TRSError]
 checkVariableNames lines = 
     [ InvalidVariableName var lineNum
     | (lineNum, line) <- lines,
-        isPrefixOf "(rule" line,
-        let rule = parseRule line,
-        Just (lhs, rhs) <- [rule],
-        let vars = nub (collectVars lhs ++ collectVars rhs),
-        var <- vars,
-        any (\c -> c `elem` ['(', ')', ';']) var
+      isPrefixOf "(rule" line,
+      let rule = parseRule line,
+      Just (lhs, rhs, _) <- [rule],
+      let vars = nub (collectVars lhs ++ collectVars rhs),
+      var <- vars,
+      any (\c -> c `elem` ['(', ')', ';']) var
     ]
 
 parseTRS :: String -> TRS
@@ -169,30 +180,47 @@ parseFun s =
     in (parts !! 1, read (parts !! 2))
 
 parseRule :: String -> Maybe Rule
-parseRule s = do
+parseRule s = 
     let withoutOuterParens = init (tail (trim s))
-        afterRule = drop 4 withoutOuterParens
-        terms = splitTermsInRule (trim afterRule)
-    case terms of
-        [lhs, rhs] -> Just (parseTerm lhs, parseTerm rhs)
+        afterRule = dropWhile isSpace (drop 4 withoutOuterParens)
+        terms = splitTermsInRule afterRule
+    in case terms of
+        [lhs, rhs] -> Just (parseTerm lhs, parseTerm rhs, Nothing)
+        [lhs, rhs, cost] | trim cost == ":cost 0" -> Just (parseTerm lhs, parseTerm rhs, Just 0)
         _ -> Nothing
 
+splitAtCost :: String -> (String, Maybe String)
+splitAtCost input = 
+    case break (==':') input of
+        (terms, ':':rest) | "cost" `isPrefixOf` rest -> 
+            let costPart = drop 4 rest
+                costValue = trim costPart
+            in (trim terms, Just (":cost " ++ costValue))
+        _ -> (trim input, Nothing)
+
 splitTermsInRule :: String -> [String]
-splitTermsInRule = go 0 "" []
+splitTermsInRule input = 
+    case splitAtCost input of
+        (termPart, Just costPart) -> parseTerms termPart ++ [costPart]
+        (termPart, Nothing) -> parseTerms termPart
   where
-    go :: Int -> String -> [String] -> String -> [String]
-    go depth acc terms [] = 
-        if null acc 
-        then reverse terms 
-        else reverse (reverse acc : terms)
-    go depth acc terms (c:cs) = case c of
-        '(' -> go (depth + 1) (c:acc) terms cs
-        ')' -> let newDepth = depth - 1
-               in if newDepth == 0 
-                  then go newDepth "" (reverse (c:acc) : terms) cs
-                  else go newDepth (c:acc) terms cs
-        ' ' | depth == 0 && not (null acc) -> go depth "" (reverse acc : terms) cs
-        _ -> go depth (c:acc) terms cs
+    parseTerms = go 0 "" []
+      where
+        go depth acc terms [] = 
+            if null acc 
+            then reverse terms 
+            else reverse (trim (reverse acc) : terms)
+        go depth acc terms (c:cs) = case c of
+            '(' -> go (depth + 1) (c:acc) terms cs
+            ')' -> let newDepth = depth - 1
+                   in if newDepth == 0 
+                      then go newDepth "" (trim (reverse (c:acc)) : terms) cs
+                      else go newDepth (c:acc) terms cs
+            ' ' | depth == 0 -> 
+                if null acc
+                then go depth "" terms cs
+                else go depth "" (trim (reverse acc) : terms) cs
+            _ -> go depth (c:acc) terms cs
 
 parseTerm :: String -> Term
 parseTerm s = 
@@ -212,23 +240,18 @@ trim = dropWhile isSpace.reverse.dropWhile isSpace.reverse
 normalizeTRS :: TRS -> TRS
 normalizeTRS trs = 
     let rulesWithFuns = [(rule, getFunsInRule rule) | rule <- rules trs]
-        
         getFunPattern fname = concat [getRuleStructure rule | (rule, funs) <- rulesWithFuns, 
                                 fname `elem` funs] ++ 
                     show [funs | (rule, funs) <- rulesWithFuns,
                         fname `elem` funs]
-
         funsByArityAndStructure = Map.fromListWith (++) 
             [(arity, [(name, getFunPattern name)]) 
             | (name, arity) <- funs trs]
-
         sortedFuns = Map.map (sortBy (\a b -> compare (snd a) (snd b))) 
                     funsByArityAndStructure
-
         funMap = Map.fromList 
             [(name, "f" ++ show arity ++ "_" ++ show rank)
             | (arity, names) <- Map.toList sortedFuns, ((name, _), rank) <- zip names [1..]]
-
         normalizedRules = sort (map (normalizeRule funMap) (rules trs))
         normalizedFuns = sort (map (\(f,a) -> (Map.findWithDefault f f funMap, a)) 
                         (funs trs))
@@ -236,10 +259,12 @@ normalizeTRS trs =
     in TRS normalizedFuns normalizedRules newHash
 
 normalizeRule :: Map.Map String String -> Rule -> Rule
-normalizeRule funMap (lhs, rhs) = 
+normalizeRule funMap (lhs, rhs, cost) = 
     let vars = nub (collectVars lhs ++ collectVars rhs)
         varMap = Map.fromList (zip vars (map (\i -> "x" ++ show i) [1..]))
-    in (normalizeTerm funMap varMap lhs, normalizeTerm funMap varMap rhs)
+    in (normalizeTerm funMap varMap lhs, 
+        normalizeTerm funMap varMap rhs,
+        cost)
 
 collectVars :: Term -> [String]
 collectVars (Var v) = [v]
@@ -253,25 +278,24 @@ normalizeTerm funMap varMap term = case term of
     Fun f ts -> Fun (Map.findWithDefault f f funMap) 
                     (map (normalizeTerm funMap varMap) ts)
 
-reportErrors :: [TRSError] -> IO ()
-reportErrors [] = return ()
-reportErrors errors = do
-    putStrLn "Found errors in TRS:"
+reportErrors :: Handle -> [TRSError] -> IO ()
+reportErrors handle errors = do
+    hPutStrLn handle "Found errors in TRS:"
     forM_ errors (\error -> case error of
         InvalidFunctionName fname line ->
-            printf "Line %d: Invalid function name '%s' (contains invalid characters)\n" 
+            hPrintf handle "Line %d: Invalid function name '%s' (contains invalid characters)\n" 
                 line fname
         InvalidVariableName vname line ->
-            printf "Line %d: Invalid variable name '%s' (contains invalid characters)\n" 
+            hPrintf handle "Line %d: Invalid variable name '%s' (contains invalid characters)\n" 
                 line vname
         InvalidArity fname line arity ->
-            printf "Line %d: Invalid arity %s for function '%s'\n" 
+            hPrintf handle "Line %d: Invalid arity %s for function '%s'\n" 
                 line arity fname
         InvalidFunctionStructure fname line content ->
-            printf "Line %d: Invalid function declaration structure: %s\n" 
+            hPrintf handle "Line %d: Invalid function declaration structure: %s\n" 
                 line content
         InvalidRuleStructure line content ->
-            printf "Line %d: Invalid rule structure: %s\n" 
+            hPrintf handle "Line %d: Invalid rule structure: %s\n" 
                 line content)
 
 findAriFiles :: FilePath -> IO [FilePath]
@@ -292,38 +316,32 @@ groupByContent :: [(FilePath, BS.ByteString)] -> [[FilePath]]
 groupByContent files = 
     let normalizedFiles = map (\(fp, content) -> 
             (fp, normalizeTRS (parseTRS (BSC.unpack content)))) files
-        
         buildGroups [] groups = groups
         buildGroups ((fp, trs):rest) groups =
             let groups' = if HM.member trs groups
                          then HM.adjust (fp:) trs groups
                          else HM.insert trs [fp] groups
             in buildGroups rest groups'
-
         groups = buildGroups normalizedFiles HM.empty
         dupGroups = HM.elems groups
     in filter (\g -> length g > 1) dupGroups
 
-printOutput :: [[FilePath]] -> IO ()
-printOutput groups = do
-    let groupCount = length groups
-    putStrLn ("Found " ++ show groupCount ++ " groups of duplicate files")
-    writeFile "duplicate_files.txt" ""
-    forM_ groups (\group -> do
-        mapM_ (\path -> appendFile "duplicate_files.txt" (path ++ "\n")) group
-        appendFile "duplicate_files.txt" "\n")
-    putStrLn "Results have been written to duplicate_files.txt"
+printOutputToHandle handle [group] = mapM_ (hPutStrLn handle) group
+printOutputToHandle handle (group:groups) = do
+    mapM_ (hPutStrLn handle) group
+    hPutStrLn handle ""
+    printOutputToHandle handle groups
 
-processWithChecks :: FilePath -> BS.ByteString -> IO (Maybe TRS)
-processWithChecks filepath content = do
+processWithChecks :: Handle -> FilePath -> BS.ByteString -> IO (Maybe TRS)
+processWithChecks handle filepath content = do
     let input = BSC.unpack content
     case checkTRS input of
         [] -> do
             let result = parseTRS input
             return (Just result)
         errors -> do
-            putStrLn ("\nErrors in file: " ++ filepath)
-            reportErrors errors
+            hPutStrLn handle ("\nErrors in file: " ++ filepath)
+            reportErrors handle errors
             return Nothing
 
 main :: IO ()
@@ -331,13 +349,18 @@ main = do
     args <- getArgs
     case args of
         [dir] -> do
+            let (duplicateFile, errorFile, performanceFile) = generateOutputNames dir
+            
+            errorHandle <- openFile errorFile WriteMode
+            hSetBuffering errorHandle LineBuffering
+
             startTime <- getCurrentTime
             files <- findAriFiles dir
             putStrLn ("Found " ++ show (length files) ++ " .ari files")
             fileContents <- forM (zip [1..] files) (\(idx, f) -> do
                 reportProgress "Reading files" idx (length files)
                 content <- BS.readFile f
-                processed <- processWithChecks f content
+                processed <- processWithChecks errorHandle f content
                 return (case processed of 
                     Just _ -> Just (f, content)
                     Nothing -> Nothing))
@@ -345,11 +368,17 @@ main = do
             reportProgress "Processing" (length validFiles) (length validFiles)
             
             let groups = groupByContent validFiles
-            printOutput groups
+            withFile duplicateFile WriteMode (\dupHandle -> do
+                printOutputToHandle dupHandle groups)
             
             endTime <- getCurrentTime
             let duration = diffUTCTime endTime startTime
             printf "\nProcessing completed in %.2f seconds\n" 
                 (realToFrac duration :: Double)
+            printf "Results written to %s\n" duplicateFile
+            printf "Errors written to %s\n" errorFile
+            printf "Performance information written to %s\n" performanceFile
+            
+            hClose errorHandle
         
         _ -> putStrLn "Error: Wrong input"
